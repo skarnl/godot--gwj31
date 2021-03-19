e071eb3d9fe93271b2fa88788c555c6536731fd1
Oskar van Velden <oskar@rakso.nl>
2021-03-19 11:01:00 +0100

    Setup basic interaction with different panels

--- screens/main_menu/options.gd
+++ screens/main_menu/options.gd
@@ -1,12 +1,12 @@
 extends Control
 
-signal options_closed
+signal close_options
 
 onready var back_button := $BackButton
 onready var key_list := $Content/ScrollContainer/KeyList
 
 func _ready() -> void:
-	back_button.connect('pressed', self, 'close')
+	back_button.connect('pressed', self, '_on_back_to_main_pressed')
 	hide()
 	
 	build_key_list()
@@ -22,8 +22,8 @@ func build_key_list() -> void:
 	
 func open() -> void:
 	show()
+	key_list.focus()
 	
 	
-func close() -> void:
-	hide()
-	emit_signal('options_closed')
+func _on_back_to_main_pressed() -> void:
+	emit_signal('close_options')
